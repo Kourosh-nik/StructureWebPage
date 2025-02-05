df04aaf8df94a7b94f9d4f73e97a9bce29f0d594
aminkhoshsirat <aminkhoshsirat1381@gmail.com>
2025-02-05 19:35:13 +0330

    complate header.html

--- Apps/Home_Page/views.py
+++ Apps/Home_Page/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import ListView, View
+from .models import *
+from Apps.BIM.models import BIMCategory
+from Apps.project_management.models import ProjManCategory
+from Apps.Retrofit.models import RetroCategory
+from Apps.Software.models import SoftCategory
+from Apps.Structure_Design.models import STRCategory
 
 
 class HeaderView(View):
     def get(self, request):
-        return render(request, 'partial/header.html')
+        panel = SiteDetailModel.objects.first()
+
+        # Categories of Menu Bar
+        bim_categories = BIMCategory.objects.all()
+        proj_categories = ProjManCategory.objects.all()
+        retro_categories = RetroCategory.objects.all()
+        str_categories = STRCategory.objects.all()
+        soft_categories = SoftCategory.objects.all()
+
+        context ={
+            'panel': panel,
+            'bim_categories': bim_categories,
+            'proj_categories': proj_categories,
+            'retro_categories': retro_categories,
+            'str_categories': str_categories,
+            'soft_categories': soft_categories,
+        }
+        return render(request, 'partial/header.html', context)
 
 class FooterView(View):
     def get(self, request):
